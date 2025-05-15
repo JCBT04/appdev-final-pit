fc5c0cb94e871a8de5350d9a05ac6b11b4a68111
jcbt <tevesjhunel01@gmail.com>
2025-05-15 23:29:51 +0800

    host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,11 @@ SECRET_KEY = 'django-insecure-qj@(-$p7-b@9zv1fcwg%1fqflx1oa6^!-hzu-#=%u*bkq&a*()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+     'appdev-final-pit.onrender.com',
+     'localhost',
+     '127.0.0.1'
+]
 
 
 # Application definition
